9c8d9220ee838c86dcc92727315ca427567c6bad
hawkridge <berya9999@gmail.com>
2022-07-25 15:16:20 +0300

    test 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BehaviorSubject, interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  val = interval(1000);
   title = 'sandbox';
   user = {
     name: 'Sorax',
